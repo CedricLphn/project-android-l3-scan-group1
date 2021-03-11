f5417f30387917ce076e2be0d174b530525747b5
Leprohon <cedric.leprohon@yahoo.fr>
2021-03-11 23:51:01 +0100

    feat: recyclerview partially done

--- app/src/main/java/com/example/projetphoto/db/AppDatabase.kt
+++ app/src/main/java/com/example/projetphoto/db/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.projetphoto.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.projetphoto.db.objects.Objects
 import com.example.projetphoto.db.objects.ObjectsDao
@@ -12,4 +14,11 @@ import com.example.projetphoto.db.pictures.PicturesDao
 abstract class AppDatabase : RoomDatabase() {
     abstract fun picturesDao(): PicturesDao
     abstract fun objectDao() : ObjectsDao
+}
+
+fun db_init(applicationContext: Context): AppDatabase {
+    return Room.databaseBuilder(
+        applicationContext,
+        AppDatabase::class.java, "test1"
+    ).allowMainThreadQueries().build()
 }
--- app/src/main/java/com/example/projetphoto/pictureList/PictureAdapter.kt
+++ app/src/main/java/com/example/projetphoto/pictureList/PictureAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.projetphoto.pictureList
 
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetphoto.databinding.ItemPictureBinding
+import com.example.projetphoto.db.pictures.Pictures
 import com.example.projetphoto.itemDetails.ItemDetailsActivity
 
-class PictureAdapter(private var pictures: List<Picture>) :
+class PictureAdapter(private var pictures: List<Pictures>) :
     RecyclerView.Adapter<PictureAdapter.ViewHolder>() {
     class ViewHolder(val binding: ItemPictureBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -28,15 +30,15 @@ class PictureAdapter(private var pictures: List<Picture>) :
                 context.startActivity(intent)
             }
             titleTextView.text = picture.title
-            dateTextView.text = picture.pictureDate
-            pictureImageView.setImageResource(picture.pictureImage)
-            nbObjectTextView.text = picture.nbObject.toString()
+            dateTextView.text = "picture.pictureDate"
+            pictureImageView.setImageURI(Uri.parse(picture.link))
+            nbObjectTextView.text = "picture.nbObject.toString()"
         }
     }
 
     override fun getItemCount(): Int = pictures.size
 
-    fun updateDataSet(photos: List<Picture>) {
+    fun updateDataSet(photos: List<Pictures>) {
         this.pictures = photos
         notifyDataSetChanged()
     }
