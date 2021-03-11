3c9194a76652c720a3630bd99c2baf2b32254b8f
Tristan Gualini <tristan.gualini.pro@gmail.com>
2021-03-11 22:39:49 +0100

    Details Picture

--- app/src/main/java/com/example/projetphoto/db/objects/ObjectsDao.kt
+++ app/src/main/java/com/example/projetphoto/db/objects/ObjectsDao.kt
@@ -9,6 +9,9 @@ interface ObjectsDao {
     @Query("SELECT * FROM Objects")
     fun getAll() : List<Objects>
 
+    @Query("SELECT * FROM Objects WHERE photo_id = :photo_id")
+    fun getObjects(photo_id : Int) : List<Objects>
+
     @Insert
     fun insert(obj : Objects)
 
--- app/src/main/java/com/example/projetphoto/db/pictures/PicturesDao.kt
+++ app/src/main/java/com/example/projetphoto/db/pictures/PicturesDao.kt
@@ -9,6 +9,9 @@ interface PicturesDao {
     @Query("SELECT * FROM pictures")
     fun getAll() : List<Pictures>
 
+    @Query("SELECT * FROM pictures WHERE id = :id")
+    fun getPicture(id: Int) : Pictures
+
     @Insert
     fun insert(picture : Pictures)
 }
