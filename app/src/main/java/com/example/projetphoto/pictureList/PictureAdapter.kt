0b47e7e9886a18b85b5fd01571811d8ae797ef27
Leprohon <cedric.leprohon@yahoo.fr>
2021-03-12 09:34:56 +0100

    feat: order by desc

--- app/src/main/java/com/example/projetphoto/pictureList/PictureAdapter.kt
+++ app/src/main/java/com/example/projetphoto/pictureList/PictureAdapter.kt
@@ -2,6 +2,7 @@ package com.example.projetphoto.pictureList
 
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.startActivity
